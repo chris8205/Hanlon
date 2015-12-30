704c1005d9bcb1d16af0c1aafdbd40332c06e617
chris8205 <info@slater.be>
2015-12-30 21:52:09 +0100

    Update hanlon-discover.ps1
    Fixed $prettyUUID byte-order

--- scripts/winpe/hanlon-discover.ps1
+++ scripts/winpe/hanlon-discover.ps1
@@ -16,14 +16,18 @@ Param (
 	[Parameter(Mandatory=$True)]
 	[String]
 	$rawUUID
-)
+)
+
 
 # Create an array of each half (hyphen delimiter)
-$octets = $rawUUID.Split("-")
+$octets = $rawUUID.Split("-")
+
 # Create an array of each two-charactere byte (space delimiter)
-$bytes = $octets[0].Split(" ") + $octets[1].Split(" ")
+$bytes = $octets[0].Split(" ") + $octets[1].Split(" ")
+
 # Build the final string, piecing together byte by byte
-$prettyUUID = $bytes[0] + $bytes[1] + $bytes[2] + $bytes[3] + "-" + $bytes[4] + $bytes[5] + "-" + $bytes[6] + $bytes[7] + "-" + $bytes[8] + $bytes[9] + "-" + $bytes[10] + $bytes[11] + $bytes[12] + $bytes[13] + $bytes[14] + $bytes[15]
+$prettyUUID = $bytes[3] + $bytes[2] + $bytes[1] + $bytes[0] + "-" + $bytes[5] + $bytes[4] + "-" + $bytes[7] + $bytes[6] + "-" + $bytes[8] + $bytes[9] + "-" + $bytes[10] + $bytes[11] + $bytes[12] + $bytes[13] + $bytes[14] + $bytes[15]
+
 Return $prettyUUID
 
 }
